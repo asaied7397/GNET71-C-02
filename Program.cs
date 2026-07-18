fb70cede8be7ee80055fd4a8b9b36b7c8face6a3
asaied7397 <ahmed.saied@ritec.global>
2026-07-18 06:33:21 +0300

    initialize project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpBasicsSession02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01 
+            Book firstBook = new Book
+            {
+                Title = "The Hobbit",
+                Pages = 464
+            };
+            Console.WriteLine("Question 01:");
+            Console.WriteLine(firstBook.ToString());
+            #endregion
+        }
+
+    }
+
+}
