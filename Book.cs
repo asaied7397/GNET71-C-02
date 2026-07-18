07cb7609373e79a463ba2204568b94865ac773f7
asaied7397 <ahmed.saied@ritec.global>
2026-07-18 08:04:59 +0300

    edit book conflict

--- Book.cs
+++ Book.cs
@@ -3,7 +3,7 @@ namespace CSharpBasicsSession02
     internal class Book
     {
         public string Title = string.Empty;
-        public int Pages;
+        public int Pages = 0;
         public override string ToString()
         {
             return $"Title: {Title}, Pages: {Pages}";
